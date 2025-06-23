2eb215345a7f637b33855cbc8a0aa0bd62bd327e
haninmohamed523 <hanin0504681287@gmail.com>
2025-06-23 04:13:47 +0300

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,24 @@
+console.log('Game');
+let clue1 = 'Ahmed';
+let clue2 = 'Ali';
+let clue3 = 'mohamed';
+let clue = '';
+let attemp = true;
+
+console.log('Choose clue (Ahmed, Ali, mohamed):');
+
+while (attemp == true) {
+    clue = prompt("Enter your guess:"); 
+    console.log(clue);
+    if (clue == clue1) {
+        console.log('Ahmed is the correct answer');
+    } else if (clue == clue2) {
+        console.log('Ali is the wrong answer');
+    } else if(clue==clue3) {
+        console.log('Mohamed is the wrong answer');
+    }
+    else{
+              console.log('not valid');
+    }
+    attemp++
+}
